822f75eb041d383c8f93c6483ee7358213a74128
Olaf Meyer <external.olaf.meyer@systecs.com>
2024-06-18 13:14:16 +0200

    Renamed class.

--- Source/Scotec.Revit/CommandAvailability.cs
+++ Source/Scotec.Revit/CommandAvailability.cs
@@ -12,42 +12,7 @@ namespace Scotec.Revit;
 
 /// <summary>
 /// </summary>
-public abstract class CommandAvailability : IExternalCommandAvailability
+[Obsolete("This class is deprecated and will be removed in future versions. Please use RevitCommandAvailability instead.")]
+public abstract class CommandAvailability : RevitCommandAvailability
 {
-    /// <inheritdoc />
-    bool IExternalCommandAvailability.IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
-    {
-        try
-        {
-            var document = applicationData.ActiveUIDocument?.Document;
-
-            using var scope = RevitApp.GetServiceProvider(applicationData.ActiveAddInId.GetGUID())
-                                      .GetAutofacRoot()
-                                      .BeginLifetimeScope(builder =>
-                                      {
-                                          if (document != null)
-                                          {
-                                              builder.RegisterInstance(document).ExternallyOwned();
-                                          }
-
-                                          builder.RegisterInstance(applicationData).ExternallyOwned();
-                                          builder.RegisterInstance(applicationData.Application).ExternallyOwned();
-                                          builder.RegisterInstance(selectedCategories).ExternallyOwned();
-                                      });
-
-            var serviceProvider = scope.Resolve<IServiceProvider>();
-            var result = IsCommandAvailable(applicationData, selectedCategories, serviceProvider);
-
-            return result;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
-    }
-
-    /// <summary>
-    ///     Implement this method to provide control over whether your external command is enabled or disabled.
-    /// </summary>
-    protected abstract bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories, IServiceProvider services);
 }
