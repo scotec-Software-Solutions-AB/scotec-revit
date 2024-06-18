9462edd7e117bf0a91e7437223bf0768076658aa
Olaf Meyer <olaf.meyer@scotec-software.com>
2024-06-19 01:34:43 +0200

    Added classes for command availability.

---
+++ Source/Scotec.Revit.Test/RevitTestCommandAvailability.cs
@@ -0,0 +1,21 @@
+﻿// Copyright © 2023 - 2024 Olaf Meyer
+// Copyright © 2023 - 2024 scotec Software Solutions AB, www.scotec-software.com
+// This file is licensed to you under the MIT license.
+
+using System;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Scotec.Revit;
+
+namespace Scotec.Revit.Test;
+
+[RevitCommandAvailabilityAttribute]
+public class RevitTestCommandAvailability : RevitCommandAvailability
+{
+    protected override bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories,
+                                             IServiceProvider services)
+    {
+        return true;
+    }
+}
