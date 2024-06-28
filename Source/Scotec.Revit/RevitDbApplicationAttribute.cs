5d4c2ac97ad63d558439b84316689c1fd0729a60
Olaf Meyer <olaf.meyer@scotec-software.com>
2024-06-28 03:53:31 +0200

    Renamed attribute. Added attribute for DbApplication.

---
+++ Source/Scotec.Revit/RevitDbApplicationAttribute.cs
@@ -0,0 +1,30 @@
+﻿// Copyright © 2023 - 2024 Olaf Meyer
+// Copyright © 2023 - 2024 scotec Software Solutions AB, www.scotec-software.com
+// This file is licensed to you under the MIT license.
+
+using System;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+
+namespace Scotec.Revit;
+
+/// <summary>
+/// 
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class RevitDbApplicationAttribute : Attribute
+{
+}
+
+public class test : IExternalDBApplication
+{
+    public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+    {
+        throw new NotImplementedException();
+    }
+}
