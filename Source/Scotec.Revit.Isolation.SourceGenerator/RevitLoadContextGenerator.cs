c28ac1a3028ce878f23a2aa3d88cd79bf53b7de2
Olaf Meyer <olaf.meyer@scotec-software.com>
2026-05-01 16:07:15 +0200

    Refactor Revit add-in isolation framework for improved configurability (#89)
    * Refactor Revit add-in isolation framework
    
    Improved encapsulation and configurability by replacing direct
    assignments with methods for managing shared and blacklisted
    assemblies. Introduced `RevitGeneratorOptions` to centralize
    configuration and added dynamic retrieval of build properties
    via `GetBuildProperty`.
    
    Updated templates to support dynamic root assembly configuration
    and enhanced initialization logic. Refactored `RevitLoadContextGenerator`
    to use `RevitGeneratorOptions` and added helper methods for
    template formatting.
    
    Reorganized project files to include transitive build properties
    via `Scotec.Revit.Isolation.props`. Updated dependencies and
    cleaned up unused references. Improved maintainability and
    extensibility of the framework.
    
    * Cleaned up unused commented-out `<ItemGroup>` in `GenerateTest.csproj` to improve readability.
    
    Added `Scotec.Revit.Isolation.Sample.props` .
    
    Detailed comments were added to the `.props` file to explain usage and important considerations.

--- Source/Scotec.Revit.Isolation.SourceGenerator/RevitLoadContextGenerator.cs
+++ Source/Scotec.Revit.Isolation.SourceGenerator/RevitLoadContextGenerator.cs
@@ -2,9 +2,31 @@
 // Copyright © 2023 - 2026 scotec Software Solutions AB, www.scotec.com
 // This file is licensed to you under the MIT license.
 
+using System.Diagnostics;
+using System.Text;
 using Microsoft.CodeAnalysis;
+using Scotec.Revit.Isolation.SourceGenerator;
 
 namespace Scotec.Revit.Isolation.SourceGenerator;
+
+internal sealed record RevitGeneratorOptions
+{
+    public RevitGeneratorOptions(string sharedAssemblies, string blackListedAssemblies, string preloadedAssemblies
+        , string addinRootAssembly, string sharedRootAssembly)
+    {
+        SharedAssemblies = sharedAssemblies.Split(['|'], StringSplitOptions.RemoveEmptyEntries);
+        BlackListedAssemblies = blackListedAssemblies.Split(['|'], StringSplitOptions.RemoveEmptyEntries);
+        PreloadedAssemblies = preloadedAssemblies.Split(['|'], StringSplitOptions.RemoveEmptyEntries);
+        AddinRootAssembly = addinRootAssembly;
+        SharedRootAssembly = sharedRootAssembly;
+    }
+    public string[] SharedAssemblies { get; set; }
+    public string[] BlackListedAssemblies { get; set; }
+    public string[] PreloadedAssemblies { get; set; }
+    public string AddinRootAssembly { get; set; }
+    public string SharedRootAssembly { get; set; }
+}
+
 
 /// <summary>
 ///     Generates source code for managing Revit add-in load contexts.
@@ -26,11 +48,39 @@ public sealed class RevitLoadContextGenerator : RevitIncrementalGenerator
     ///     <see cref="IncrementalGeneratorInitializationContext.CompilationProvider" />
     ///     to produce source files dynamically.
     /// </remarks>
+    //protected override void OnInitialize()
+    //{
+    //    Context.RegisterSourceOutput(Context.CompilationProvider, Execute);
+    //}
     protected override void OnInitialize()
     {
-        Context.RegisterSourceOutput(Context.CompilationProvider, Execute);
-    }
+        var sharedAssemblies = GetBuildProperty("RevitLoadContextSharedAssemblies");
+        var blackListedAssemblies = GetBuildProperty("RevitLoadContextBlackListedAssemblies");
+        var preloadedAssemblies = GetBuildProperty("RevitLoadContextPreloadedAssemblies");
+        var addinRootAssembly = GetBuildProperty("RevitLoadContextAddinRootAssembly");
+        var sharedRootAssembly = GetBuildProperty("RevitLoadContextSharedRootAssembly");
 
+        var options =
+            sharedAssemblies
+                .Combine(blackListedAssemblies)
+                .Combine(preloadedAssemblies)
+                .Combine(addinRootAssembly)
+                .Combine(sharedRootAssembly)
+                .Select((x, _) => new RevitGeneratorOptions(
+                    sharedAssemblies: x.Left.Left.Left.Left ?? string.Empty,
+                    blackListedAssemblies: x.Left.Left.Left.Right ?? string.Empty,
+                    preloadedAssemblies: x.Left.Left.Right ?? string.Empty,
+                    addinRootAssembly: x.Left.Right ?? string.Empty,
+                    sharedRootAssembly: x.Right ?? string.Empty)); var input = Context.CompilationProvider.Combine(options);
+
+        Context.RegisterSourceOutput(input,  (context, source) =>
+        {
+            var compilation = source.Left;
+            var generatorOptions = source.Right;
+
+            Execute(context, compilation, generatorOptions);
+        });
+    }
     /// <summary>
     ///     Executes the source generation process for creating the Revit add-in load context.
     /// </summary>
@@ -45,11 +95,11 @@ public sealed class RevitLoadContextGenerator : RevitIncrementalGenerator
     ///     and formatting it with the namespace of the current assembly. The generated source file is then added
     ///     to the compilation output.
     /// </remarks>
-    private void Execute(SourceProductionContext context, Compilation compilation)
+    private void Execute(SourceProductionContext context, Compilation compilation, RevitGeneratorOptions generatorOptions)
     {
         var @namespace = compilation.Assembly.Name;
 
-        var hasAddinContext = TryGenerateAddinLoadContext(context, compilation, @namespace, out var contextName, out var useSharedContext);
+        var hasAddinContext = TryGenerateAddinLoadContext(context, compilation, generatorOptions, @namespace, out var contextName, out var useSharedContext);
         var hasSharedContext = TryGenerateAddinSharedLoadContext(context, compilation, @namespace, out var sharedContextName);
 
         if ((!hasAddinContext || string.IsNullOrEmpty(contextName)) && (!hasSharedContext || string.IsNullOrEmpty(sharedContextName)))
@@ -63,21 +113,23 @@ public sealed class RevitLoadContextGenerator : RevitIncrementalGenerator
         sharedContextName ??= string.Empty;
         useSharedContext ??= string.Empty;
 
-        GenerateContextInitializer(context, @namespace, sharedContextName, contextName, useSharedContext, hasAddinContext, hasSharedContext);
+        GenerateContextInitializer(context, generatorOptions, @namespace, sharedContextName, contextName, useSharedContext, hasAddinContext, hasSharedContext);
     }
 
-    private static void GenerateContextInitializer(SourceProductionContext context, string @namespace, string sharedContextName, string contextName,
+    private static void GenerateContextInitializer(SourceProductionContext context, RevitGeneratorOptions generatorOptions, string @namespace, string sharedContextName, string contextName,
                                                    string usedSharedContextName, bool hasAddinContext, bool hasSharedContext)
     {
         var template = LoadTemplate("RevitAssemblyLoadContextInitializer");
         if (!string.IsNullOrEmpty(template))
         {
-            var content = string.Format(template, @namespace, sharedContextName, contextName, usedSharedContextName, hasAddinContext.ToString(), hasSharedContext.ToString());
+            var content = string.Format(template, @namespace, sharedContextName, contextName, 
+                usedSharedContextName, hasAddinContext.ToString(), hasSharedContext.ToString(),
+                generatorOptions.AddinRootAssembly, generatorOptions.SharedRootAssembly);
             context.AddSource("RevitAssemblyLoadContextInitializer.g.cs", content);
         }
     }
 
-    private bool TryGenerateAddinLoadContext(SourceProductionContext context, Compilation compilation, string @namespace, out string? contextName,
+    private bool TryGenerateAddinLoadContext(SourceProductionContext context, Compilation compilation, RevitGeneratorOptions generatorOptions, string @namespace, out string? contextName,
                                              out string? sharedContextName)
     {
         if (!TryGetRevitAddinContextName(compilation, out contextName, out sharedContextName) || string.IsNullOrEmpty(contextName))
@@ -88,12 +140,27 @@ public sealed class RevitLoadContextGenerator : RevitIncrementalGenerator
         var template = LoadTemplate("RevitAddinAssemblyLoadContext");
         if (!string.IsNullOrEmpty(template))
         {
-            var content = string.Format(template, @namespace);
+            var content = string.Format(template, @namespace, 
+                generatorOptions.AddinRootAssembly,
+                BuildCollectionExpression(generatorOptions.SharedAssemblies),
+                BuildCollectionExpression(generatorOptions.BlackListedAssemblies),
+                BuildCollectionExpression(generatorOptions.PreloadedAssemblies));
             context.AddSource("RevitAddinAssemblyLoadContext.g.cs", content);
             return true;
         }
 
         return false;
+    }
+
+    private static string BuildCollectionExpression(string[] values)
+    {
+        var builder = new StringBuilder();
+        builder.Append('[');
+
+        builder.Append(string.Join(",", values.Select(v => $"\"{v}\"")));
+        builder.Append(']');
+
+        return builder.ToString();
     }
 
     private bool TryGenerateAddinSharedLoadContext(SourceProductionContext context, Compilation compilation, string @namespace, out string? contextName)
